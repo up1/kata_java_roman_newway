fb92878062b2b591d175b63f163dd255ad9c33e3
somkiat <somkiat.p@gmail.com>
2014-06-10 18:24:16 +0700

    Add second test for 1 -> I

--- src/test/java/kata/roman/number/RomanNumberTest.java
+++ src/test/java/kata/roman/number/RomanNumberTest.java
@@ -11,5 +11,11 @@ public class RomanNumberTest {
 		RomanNumber romanNumber = new RomanNumber();
 		assertEquals("", romanNumber.convert(0));
 	}
+	
+	@Test
+	public void oneShouldReturnI() {
+		RomanNumber romanNumber = new RomanNumber();
+		assertEquals("I", romanNumber.convert(1));
+	}
 
 }
