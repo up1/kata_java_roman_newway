cd987c01571863c1e894cd3213f5d9340c2ddcb7
somkiat <somkiat.p@gmail.com>
2014-06-10 23:17:36 +0700

    Refactor code with duplicate problem, My solution :: use resusive function

--- src/main/java/kata/roman/number/RomanNumber.java
+++ src/main/java/kata/roman/number/RomanNumber.java
@@ -3,16 +3,16 @@ package kata.roman.number;
 public class RomanNumber {
 
 	public String convert(int arabicNumber) {
+		if(arabicNumber == 0) {
+			return "";
+		}
 		if(arabicNumber == 1) {
 			return "I";
 		}
 		if(arabicNumber == 5) {
 			return "V";
 		}
-		if(arabicNumber == 2) {
-			return "II";
-		}
-		return "";
+		return "I" + convert(1);
 	}
 
 }
