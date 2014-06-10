b34afe5d90315fc359917f9668fa9ce062cb95c6
somkiat <somkiat.p@gmail.com>
2014-06-10 23:29:26 +0700

    Refactor code to transform constant to variable => Array of arabic and roman

--- src/main/java/kata/roman/number/RomanNumber.java
+++ src/main/java/kata/roman/number/RomanNumber.java
@@ -1,16 +1,34 @@
 package kata.roman.number;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class RomanNumber {
 
 	public String convert(int arabicNumber) {
-		if(arabicNumber == 0) {
+		if (arabicNumber == 0) {
 			return "";
 		}
-		
-		if(arabicNumber == 5) {
+
+		if (arabicNumber == 5) {
 			return "V";
 		}
-		return "I" + convert(arabicNumber - 1);
+
+		ArrayList<MyNumber> myNumber = new ArrayList<MyNumber>();
+		myNumber.add(new MyNumber(1, "I"));
+
+		return myNumber.get(0).roman
+				+ convert(arabicNumber - myNumber.get(0).arabic);
 	}
 
+}
+
+class MyNumber {
+	public int arabic;
+	public String roman;
+
+	public MyNumber(int arabic, String roman) {
+		this.arabic = arabic;
+		this.roman = roman;
+	}
 }
